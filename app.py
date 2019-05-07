3b1acdb914f8020f5708e1733348108ac1525e5f
Simon Gavris <simon.gavris@craftworks.at>
2019-05-07 09:52:30 +0200

    feature: add application

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+import psycopg2 as psy
+
+con = psy.connect(host="127.0.0.1", database="test", user="postgres", password="secret")
+
+def create_table(connection):
+    cur = connection.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS requests (id serial PRIMARY KEY, request varchar);")
+    connection.commit()
+    cur.close()
+
+
+def write_db(connection, string):
+    cur = connection.cursor()
+    try:
+        cur.execute("INSERT INTO requests (request) VALUES ('" + string + "');")
+    except:
+        connection.rollback()
+    connection.commit()
+    cur.close()
+
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    write_db(con, path)
+    return 'Requested path: %s' % path
+
+if __name__ == '__main__':
+    create_table(con)
+    app.run(debug=True, host='0.0.0.0')
+
